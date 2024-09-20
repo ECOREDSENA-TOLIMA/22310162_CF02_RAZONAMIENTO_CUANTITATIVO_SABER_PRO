7789625ae6f552f745f749da3de567d59edf59c1
veimarcm <vcelis@sena.edu.co>
2024-09-20 08:37:03 -0500

    AD

---
+++ public/actividades/story_content/triggers.js
@@ -0,0 +1,44 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5dENYICfEQY":
+        Script1();
+        break;
+      case "6bMNCCOa9wH":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('61vrMf05ee3');
+const duration = 750;
+const easing = 'ease-out';
+const id = '668wu7q9hk8';
+const shakeAmount = 2;
+player.addForTriggers(
+id,
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
